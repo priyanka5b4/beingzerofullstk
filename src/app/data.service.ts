f5c3c48e91d0e01a23768f07634c52162879d192
priyanka5b4 <sriramakavacham.priyanka@gmail.com>
2020-01-02 17:03:31 +0530

    fetched data

---
+++ src/app/data.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  constructor(private http:HttpClient) { }
+  getData(){
+    return this.http.get('https://api.github.com/users?since=1001');
+  }
+}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+ x ="random data";
+ y:number=1;
+ z=true;
   constructor() { }
 
   ngOnInit() {
+    this.x=this.x+" new ";
+  }
+  fnclick(){
+    this.x=this.x +" new ";
   }
 
 }
